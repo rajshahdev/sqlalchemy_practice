5d860d3be32da9dee882a4216a50a3436f8f8b14
rajshahdev <be.rajshah@gmail.com>
2022-01-14 16:23:43 +0530

    adding delete file

---
+++ deleting.py
@@ -0,0 +1,8 @@
+from main import SessionLocal, engine
+from models import User, Posts
+
+local_session = SessionLocal(bind=engine)
+
+user_to_delete = local_session.query(User).filter(User.username == 'raj').first()
+local_session.delete(user_to_delete)
+local_session.commit()
