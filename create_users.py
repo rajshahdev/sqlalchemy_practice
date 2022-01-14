fbf15dd6593800a813fba64e29de387cb89041e6
rajshahdev <be.rajshah@gmail.com>
2022-01-14 16:58:40 +0530

    adding ordering file

--- create_users.py
+++ create_users.py
@@ -3,9 +3,13 @@ from models import User, Posts
 
 local_session = SessionLocal(bind=engine)
 new_user = User(username = 'rajshaasaasash', email='rajshaaasssah@gmail.com')
+diff_user = User(username = 'xoxo', email='xoxo@xoxo.com')
 new_post = Posts(title = 'rahbkbkadb dsajbdk asjkdb', description = 'ajbskabsa asbkbakbskas bkjasbajabskjabkjabkas jabskjbasjbajsbkasababskbskakbaksba kjbaskjbakbaskjbaksbkabska kjabskjabkbsakjbkabska kjabskasbajksbak')
-local_session.add(new_user)
+# local_session.add(new_user)
+# local_session.commit()
+
+local_session.add(diff_user)
 local_session.commit()
 
-local_session.add(new_post)
-local_session.commit()
+# local_session.add(new_post)
+# local_session.commit()
