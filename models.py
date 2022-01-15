ae62070b81fd6459f4fd46465c7b145561d4b999
Raj Shah <48146258+rajshahdev@users.noreply.github.com>
2022-01-15 18:41:17 +0530

    Update models.py

--- models.py
+++ models.py
@@ -11,22 +11,13 @@ class User(base):
     username = Column(String(length=30), nullable=False, unique=True)
     email = Column(String(length=30), nullable=False, unique=True)
     created_at = Column(DateTime(), server_default='now()')
-    children = relationship('Posts', backref='author',cascade="all, delete",passive_deletes=True)
 
 class Posts(base):
     __tablename__ = 'posts123'
     id = Column(Integer(), primary_key=True)
     title = Column(String(length=100), nullable=False)
     description = Column(TEXT(charset='latin1'), nullable=False)
-    user_id = Column(Integer, ForeignKey('users123.id', ondelete="CASCADE"))
 
-    # if we access the user via post table then
-    # post = session.query(Posts).filter(Post.id == 1).first()
-    # print(post.author)
-
-    # if we want to access user by User table then
-    # user = session.query(User).filter(User.id == 1).first()
-    # print(user.username)
 
 
 # class Parent(base):
